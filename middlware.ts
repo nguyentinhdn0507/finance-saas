0d7fede753dbb1a5785e620d5780e7eae87dfd7d
nguyentinh <nguyentinhdn0507@gmail.com>
2024-06-10 09:09:37 +0700

    [FINANCE SAAS] fix middleware

--- middlware.ts
+++ middlware.ts
@@ -1,13 +1,14 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+const isProtectedRoute = createRouteMatcher(["/(dashboard)", "/api(.*)"]);
 
 export default clerkMiddleware((auth, request) => {
-  if (!isPublicRoute(request)) {
+  if (isProtectedRoute(request)) {
     auth().protect();
   }
+  return NextResponse.next();
 });
-
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
